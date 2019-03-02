567dcd3d5917cb7fc407288bc963d1ffaac2e67e
sasaki.h <sasaki.h1026@gmail.com>
2019-03-02 20:04:39 +0900

    fix style

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const express = require('express');
+const port = process.env.PORT || 5000;
+const app = express({ strict: true });
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(express.static(__dirname + "/dist/"));
 
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+app.get(/.*/, function(req, res) {
+  console.log("kitayo")
+    res.sendfile(__dirname + "/dist/index.html");
+});
+
+app.listen(port);
--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI, { locale })
 
 /* eslint-disable no-new */
 new Vue({
